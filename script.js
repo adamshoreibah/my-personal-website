c657c4df4cb7a99df24be627afcb7c18d4103781
Adam Shoreibah <shoreibahadam@gmail.com>
2022-03-27 14:10:53 -0500

    final website

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h3 = document.createElement("h2");
+const h3 = document.createElement("h3");
 h3.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h3);
